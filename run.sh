16e34252f521b9c9189669b81d9a7fbc8bade160
duozhang <duozhang@iastate.edu>
2024-05-23 22:34:52 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,3 +12,4 @@ CUDA_VISIBLE_DEVICES="0" python3 client.py --partition-id=0 --task_id=0 &
 CUDA_VISIBLE_DEVICES="1" python3 client.py --partition-id=1 --task_id=1 &
 
 
+#sudo CUDA_VISIBLE_DEVICES="0" nvprof python3 client.py --partition-id=0 --task_id=0
